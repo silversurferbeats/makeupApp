009dceffc0cf591943453c148d9e1176fccdf646
silversurferbeats <gabrielsuarez89@gmail.com>
2021-08-22 15:10:46 -0300

    Updates

---
+++ precache-manifest.b8b4836bdb1a65c61f455f2051feb5d4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d904c451f779c67081d6c125a2abca0b",
+    "url": "/makeupApp/index.html"
+  },
+  {
+    "revision": "8d96cd1a3e707b880ec5",
+    "url": "/makeupApp/static/css/main.7f6d821b.chunk.css"
+  },
+  {
+    "revision": "3275eea2a69d404422a1",
+    "url": "/makeupApp/static/js/2.2cf1a246.chunk.js"
+  },
+  {
+    "revision": "33b04be6cc0808748121da507d1501cb",
+    "url": "/makeupApp/static/js/2.2cf1a246.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8d96cd1a3e707b880ec5",
+    "url": "/makeupApp/static/js/main.b2ad0dd6.chunk.js"
+  },
+  {
+    "revision": "0484332a1571aad07c5a",
+    "url": "/makeupApp/static/js/runtime-main.59fe2d35.js"
+  },
+  {
+    "revision": "a9fe1c1028b27c5eac3b2af092718b73",
+    "url": "/makeupApp/static/media/mainPic.a9fe1c10.jpg"
+  },
+  {
+    "revision": "74c00c78dccf6f2548df265b8e8659c9",
+    "url": "/makeupApp/static/media/makeup.74c00c78.png"
+  },
+  {
+    "revision": "7fde114eac5b3e30d6606e9e9b281a39",
+    "url": "/makeupApp/static/media/p1.7fde114e.jpg"
+  },
+  {
+    "revision": "0c5c13282fc136b8da0351f026a23a69",
+    "url": "/makeupApp/static/media/tip1.0c5c1328.jpg"
+  },
+  {
+    "revision": "9ffed4e7227cae5346087ee0e1f77fe5",
+    "url": "/makeupApp/static/media/tip2.9ffed4e7.jpg"
+  },
+  {
+    "revision": "8225b094e47c23dd24f6d3e5e8e9fe50",
+    "url": "/makeupApp/static/media/tip3.8225b094.jpg"
+  }
+]);
